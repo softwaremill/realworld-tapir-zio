6689d642ef4bdbded9e9fedd05b49ce0c675b73b
donut <bartek.henkiel@gmail.com>
2023-04-07 10:04:23 +0200

    Proof of concept implementation of generic repository

--- src/main/scala/com/softwaremill/realworld/profiles/ProfilesRepository.scala
+++ src/main/scala/com/softwaremill/realworld/profiles/ProfilesRepository.scala
@@ -1,28 +1,37 @@
 package com.softwaremill.realworld.profiles
 
 import io.getquill.*
+import io.getquill.context.sql.idiom.SqlIdiom
 import io.getquill.jdbczio.*
-import zio.ZLayer
+import zio.{Tag, ZIO, ZLayer}
 
+import java.sql.SQLException
 import javax.sql.DataSource
 
-class ProfilesRepository(quill: Quill.Sqlite[SnakeCase]) {
-  import quill.*
-
-  def follow(followedId: Int, followerId: Int) = run {
-    query[Followers].insert(_.userId -> lift(followedId), _.followerId -> lift(followerId)).onConflictIgnore
-  }
-
-  def unfollow(followedId: Int, followerId: Int) = run {
-    query[Followers].filter(f => (f.userId == lift(followedId)) && (f.followerId == lift(followerId))).delete
-  }
-
-  def isFollowing(followedId: Int, followerId: Int) = run {
-    query[Followers].filter(_.userId == lift(followedId)).filter(_.followerId == lift(followerId)).map(_ => 1).nonEmpty
-  }
-
+trait ProfilesRepository {
+  def follow(followedId: Int, followerId: Int): ZIO[Any, SQLException, Long]
+  def unfollow(followedId: Int, followerId: Int): ZIO[Any, SQLException, Long]
+  def isFollowing(followedId: Int, followerId: Int): ZIO[Any, SQLException, Boolean]
 }
 
-object ProfilesRepository:
-  val live: ZLayer[Quill.Sqlite[SnakeCase], Nothing, ProfilesRepository] =
-    ZLayer.fromFunction(new ProfilesRepository(_))
+object ProfilesRepository {
+  def live[I <: SqlIdiom: Tag, N <: NamingStrategy: Tag]: ZLayer[Quill[I, N], Nothing, ProfilesRepository] =
+    ZLayer.fromFunction(ProfilesRepositoryLive[I, N](_))
+
+  private class ProfilesRepositoryLive[I <: SqlIdiom, N <: NamingStrategy](quill: Quill[I, N]) extends ProfilesRepository {
+
+    import quill.*
+
+    def follow(followedId: Int, followerId: Int): ZIO[Any, SQLException, Long] = run {
+      query[Followers].insert(_.userId -> lift(followedId), _.followerId -> lift(followerId)).onConflictIgnore
+    }
+
+    def unfollow(followedId: Int, followerId: Int): ZIO[Any, SQLException, Long] = run {
+      query[Followers].filter(f => (f.userId == lift(followedId)) && (f.followerId == lift(followerId))).delete
+    }
+
+    def isFollowing(followedId: Int, followerId: Int): ZIO[Any, SQLException, Boolean] = run {
+      query[Followers].filter(_.userId == lift(followedId)).filter(_.followerId == lift(followerId)).map(_ => 1).nonEmpty
+    }
+  }
+}
