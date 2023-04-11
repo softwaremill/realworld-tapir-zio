496490ad0ec466015a21176b3182b1c99dc29daa
Adrian Wydra <wydra@adrians-macbook-pro.home>
2023-04-11 10:54:21 +0200

    Add tags endpoint

--- src/main/scala/com/softwaremill/realworld/Endpoints.scala
+++ src/main/scala/com/softwaremill/realworld/Endpoints.scala
@@ -3,15 +3,16 @@ package com.softwaremill.realworld
 import com.softwaremill.realworld.articles.ArticlesEndpoints
 import com.softwaremill.realworld.db.DbConfig
 import com.softwaremill.realworld.profiles.ProfilesEndpoints
+import com.softwaremill.realworld.tags.TagsEndpoints
 import com.softwaremill.realworld.users.UsersEndpoints
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
 import sttp.tapir.ztapir.ZServerEndpoint
 import zio.{Task, ZIO, ZLayer}
 
-class Endpoints(articlesEndpoints: ArticlesEndpoints, usersEndpoints: UsersEndpoints, profilesEndpoints: ProfilesEndpoints):
+class Endpoints(articlesEndpoints: ArticlesEndpoints, usersEndpoints: UsersEndpoints, profilesEndpoints: ProfilesEndpoints, tagsEndpoints: TagsEndpoints):
 
   val endpoints: List[ZServerEndpoint[Any, Any]] = {
-    val api = articlesEndpoints.endpoints ++ usersEndpoints.endpoints ++ profilesEndpoints.endpoints
+    val api = articlesEndpoints.endpoints ++ usersEndpoints.endpoints ++ profilesEndpoints.endpoints ++ tagsEndpoints.endpoints
     val docs = docsEndpoints(api)
     api ++ docs
   }
@@ -20,6 +21,5 @@ class Endpoints(articlesEndpoints: ArticlesEndpoints, usersEndpoints: UsersEndpo
     .fromServerEndpoints[Task](apiEndpoints, "realworld-tapir-zio", "0.1.0")
 
 object Endpoints:
-
-  val live: ZLayer[ArticlesEndpoints with UsersEndpoints with ProfilesEndpoints, Nothing, Endpoints] =
-    ZLayer.fromFunction(new Endpoints(_, _, _))
+  val live: ZLayer[ArticlesEndpoints with UsersEndpoints with ProfilesEndpoints with TagsEndpoints, Nothing, Endpoints] =
+    ZLayer.fromFunction(new Endpoints(_, _, _, _))
