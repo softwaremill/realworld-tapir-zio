ef2f71169bb22be35c1cf1aebb06d6b3d1a32c62
Adrian Wydra <wydra@Adrians-MacBook-Pro.local>
2023-04-28 11:03:52 +0200

    Add examples to article endpoints

--- src/main/scala/com/softwaremill/realworld/articles/core/api/ArticlesEndpoints.scala
+++ src/main/scala/com/softwaremill/realworld/articles/core/api/ArticlesEndpoints.scala
@@ -1,7 +1,7 @@
 package com.softwaremill.realworld.articles.core.api
 
-import com.softwaremill.realworld.articles.core.ArticlesFilters
-import com.softwaremill.realworld.common.{BaseEndpoints, Pagination}
+import com.softwaremill.realworld.articles.core.{Article, ArticleAuthor, ArticlesFilters}
+import com.softwaremill.realworld.common.{BaseEndpoints, ErrorInfo, Pagination, UserSession}
 import io.getquill.SnakeCase
 import sttp.model.StatusCode
 import sttp.tapir.generic.auto.*
@@ -11,6 +11,7 @@ import sttp.tapir.ztapir.*
 import sttp.tapir.{EndpointInput, PublicEndpoint, Validator}
 import zio.{Cause, Console, Exit, ZIO, ZLayer}
 
+import java.time.Instant
 import javax.sql.DataSource
 import scala.util.chaining.*
 
@@ -39,41 +40,111 @@ class ArticlesEndpoints(base: BaseEndpoints):
       )
       .mapTo[Pagination]
 
-  val listArticlesEndpoint = base.optionallySecureEndpoint.get
+  val listArticlesEndpoint: ZPartialServerEndpoint[Any, Option[String], Option[
+    UserSession
+  ], (ArticlesFilters, Pagination), ErrorInfo, ArticlesListResponse, Any] = base.optionallySecureEndpoint.get
     .in("api" / "articles")
     .in(articlesFilters)
     .in(articlesPagination)
-    .out(jsonBody[ArticlesListResponse])
+    .out(jsonBody[ArticlesListResponse].example(Examples.articlesListResponse))
 
-  val feedArticlesEndpoint = base.secureEndpoint.get
-    .in("api" / "articles" / "feed")
-    .in(articlesPagination)
-    .out(jsonBody[ArticlesListResponse])
+  val feedArticlesEndpoint: ZPartialServerEndpoint[Any, String, UserSession, Pagination, ErrorInfo, ArticlesListResponse, Any] =
+    base.secureEndpoint.get
+      .in("api" / "articles" / "feed")
+      .in(articlesPagination)
+      .out(jsonBody[ArticlesListResponse].example(Examples.articlesFeedResponse))
 
-  val getEndpoint = base.secureEndpoint.get
+  val getEndpoint: ZPartialServerEndpoint[Any, String, UserSession, String, ErrorInfo, ArticleResponse, Any] = base.secureEndpoint.get
     .in("api" / "articles" / path[String]("slug")) // TODO Input Validation
-    .out(jsonBody[ArticleResponse])
+    .out(jsonBody[ArticleResponse].example(Examples.articleResponse))
 
-  val createEndpoint = base.secureEndpoint.post
-    .in("api" / "articles")
-    .in(jsonBody[ArticleCreateRequest]) // TODO Input Validation
-    .out(jsonBody[ArticleResponse])
+  val createEndpoint: ZPartialServerEndpoint[Any, String, UserSession, ArticleCreateRequest, ErrorInfo, ArticleResponse, Any] =
+    base.secureEndpoint.post
+      .in("api" / "articles")
+      .in(jsonBody[ArticleCreateRequest].example(Examples.articleCreateRequest)) // TODO Input Validation
+      .out(jsonBody[ArticleResponse].example(Examples.articleResponse))
 
-  val deleteEndpoint = base.secureEndpoint.delete
+  val deleteEndpoint: ZPartialServerEndpoint[Any, String, UserSession, String, ErrorInfo, Unit, Any] = base.secureEndpoint.delete
     .in("api" / "articles" / path[String]("slug"))
 
-  val updateEndpoint = base.secureEndpoint.put
-    .in("api" / "articles" / path[String]("slug"))
-    .in(jsonBody[ArticleUpdateRequest]) // TODO Input Validation
-    .out(jsonBody[ArticleResponse])
+  val updateEndpoint: ZPartialServerEndpoint[Any, String, UserSession, (String, ArticleUpdateRequest), ErrorInfo, ArticleResponse, Any] =
+    base.secureEndpoint.put
+      .in("api" / "articles" / path[String]("slug"))
+      .in(jsonBody[ArticleUpdateRequest].example(Examples.articleUpdateRequest)) // TODO Input Validation
+      .out(jsonBody[ArticleResponse].example(Examples.articleResponse))
 
-  val makeFavoriteEndpoint = base.secureEndpoint.post
-    .in("api" / "articles" / path[String]("slug") / "favorite")
-    .out(jsonBody[ArticleResponse])
+  val makeFavoriteEndpoint: ZPartialServerEndpoint[Any, String, UserSession, String, ErrorInfo, ArticleResponse, Any] =
+    base.secureEndpoint.post
+      .in("api" / "articles" / path[String]("slug") / "favorite")
+      .out(jsonBody[ArticleResponse].example(Examples.favoriteArticleResponse))
 
-  val removeFavoriteEndpoint = base.secureEndpoint.delete
-    .in("api" / "articles" / path[String]("slug") / "favorite")
-    .out(jsonBody[ArticleResponse])
+  val removeFavoriteEndpoint: ZPartialServerEndpoint[Any, String, UserSession, String, ErrorInfo, ArticleResponse, Any] =
+    base.secureEndpoint.delete
+      .in("api" / "articles" / path[String]("slug") / "favorite")
+      .out(jsonBody[ArticleResponse].example(Examples.articleResponse))
+
+  private object Examples:
+
+    private val article1: Article = Article(
+      slug = "how-to-train-your-dragon",
+      title = "How to train your dragon",
+      description = "Ever wonder how?",
+      body = "It takes a Jacobian",
+      tagList = List("dragons", "training"),
+      createdAt = Instant.now(),
+      updatedAt = Instant.now(),
+      favorited = false,
+      favoritesCount = 2,
+      author = ArticleAuthor(username = "user1", bio = Some("user1Bio"), image = Some("user1Image"), following = false)
+    )
+
+    private val article2: Article = Article(
+      slug = "how-to-train-your-dragon-2",
+      title = "How to train your dragon 2",
+      description = "So toothless",
+      body = "Its a dragon",
+      tagList = List("dragons", "goats", "training"),
+      createdAt = Instant.now(),
+      updatedAt = Instant.now(),
+      favorited = false,
+      favoritesCount = 1,
+      author = ArticleAuthor(username = "user1", bio = Some("user1Bio"), image = Some("user1Image"), following = false)
+    )
+
+    private val article3: Article = Article(
+      slug = "how-to-train-your-dragon-3",
+      title = "How to train your dragon 3",
+      description = "So toothless",
+      body = "Its a dragon",
+      tagList = List(),
+      createdAt = Instant.now(),
+      updatedAt = Instant.now(),
+      favorited = true,
+      favoritesCount = 0,
+      author = ArticleAuthor(username = "user2", bio = Some("user2Bio"), image = Some("user2Image"), following = false)
+    )
+
+    val articleResponse: ArticleResponse = ArticleResponse(article = article1)
+    val favoriteArticleResponse: ArticleResponse = ArticleResponse(article = article3)
+    val articlesFeedResponse: ArticlesListResponse = ArticlesListResponse(articles = List(article1, article2), articlesCount = 2)
+    val articlesListResponse: ArticlesListResponse = ArticlesListResponse(articles = List(article1, article2, article3), articlesCount = 3)
+
+    val articleCreateRequest: ArticleCreateRequest = ArticleCreateRequest(article =
+      ArticleCreateData(
+        title = "How to train your dragon",
+        description = "Ever wonder how?",
+        body = "It takes a Jacobian",
+        tagList = Some(List("dragons", "training"))
+      )
+    )
+
+    val articleUpdateRequest: ArticleUpdateRequest = ArticleUpdateRequest(article =
+      ArticleUpdateData(
+        title = Some("How to train your dragon"),
+        description = Some("Ever wonder how?"),
+        body = Some("It takes a Jacobian")
+      )
+    )
 
 object ArticlesEndpoints:
   val live: ZLayer[BaseEndpoints, Nothing, ArticlesEndpoints] =
