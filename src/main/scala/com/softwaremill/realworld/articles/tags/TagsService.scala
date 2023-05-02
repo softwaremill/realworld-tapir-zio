ff9beabbf8bd19ace6fa6fa197aa232aae7e4685
Adrian Wydra <wydra@adrians-macbook-pro.home>
2023-05-02 12:27:32 +0200

    [Tag] Left rows classes only in repositories

--- src/main/scala/com/softwaremill/realworld/articles/tags/TagsService.scala
+++ src/main/scala/com/softwaremill/realworld/articles/tags/TagsService.scala
@@ -5,7 +5,7 @@ import zio.{ZIO, ZLayer}
 import java.sql.SQLException
 
 class TagsService(tagsRepository: TagsRepository):
-  def getAllTags: ZIO[Any, SQLException, List[TagRow]] = tagsRepository.listTags
+  def getAllTags: ZIO[Any, SQLException, List[String]] = tagsRepository.listTags
 
 object TagsService:
   val live: ZLayer[TagsRepository, Nothing, TagsService] = ZLayer.fromFunction(TagsService(_))
