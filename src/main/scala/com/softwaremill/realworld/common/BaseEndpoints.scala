b73a84e76525c94c07b585ade1eed3abd021612b
wydra98 <wydra.adrian98@gmail.com>
2023-05-31 11:16:07 +0200

    add bearer and token auth together

--- src/main/scala/com/softwaremill/realworld/common/BaseEndpoints.scala
+++ src/main/scala/com/softwaremill/realworld/common/BaseEndpoints.scala
@@ -13,17 +13,27 @@ import sttp.tapir.generic.auto.*
 import sttp.tapir.json.zio.jsonBody
 import sttp.tapir.server.ServerEndpoint.Full
 import sttp.tapir.ztapir.*
-import sttp.tapir.{Endpoint, EndpointIO, EndpointInput, EndpointOutput, PublicEndpoint, Validator}
+import sttp.tapir.{Codec, CodecFormat, Endpoint, EndpointIO, EndpointInput, EndpointOutput, Mapping, PublicEndpoint, Schema, TapirAuth, Validator}
 import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder}
 import zio.{Cause, Exit, IO, Task, ZIO, ZLayer}
+import sttp.model.HeaderNames
+import sttp.model.headers.{AuthenticationScheme, WWWAuthenticateChallenge}
+import sttp.tapir.CodecFormat.TextPlain
+import sttp.tapir.EndpointInput.Auth
+import sttp.tapir.generic.auto.*
 
+import scala.collection.immutable.ListMap
+import sttp.tapir.Codec.*
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 case class UserSession(userId: Int)
 
 class BaseEndpoints(authService: AuthService, usersRepository: UsersRepository):
 
   val secureEndpoint: ZPartialServerEndpoint[Any, String, UserSession, Unit, ErrorInfo, Unit, Any] = endpoint
     .errorOut(defaultErrorOutputs)
-    .securityIn(auth.http[String]("Token", WWWAuthenticateChallenge("Token")))
+    .securityIn(http[String])
     .zServerSecurityLogic[Any, UserSession](handleAuth)
 
   val optionallySecureEndpoint: ZPartialServerEndpoint[Any, Option[String], Option[UserSession], Unit, ErrorInfo, Unit, Any] = endpoint
@@ -33,6 +43,47 @@ class BaseEndpoints(authService: AuthService, usersRepository: UsersRepository):
 
   val publicEndpoint: PublicEndpoint[Unit, ErrorInfo, Unit, Any] = endpoint
     .errorOut(defaultErrorOutputs)
+
+
+  /**
+   * Mam problem z autoryzacją token albo bearer. Nie ma możliwości dokładnie przewidzieć jaka ta autoryzacja będzie,
+   * czy to bedzie token czy bearer, musimy to podejmować na bieżąco. Zauwazylem, ze dostęp do oryginalnych tokenow
+   * mamy w codecu, możemy dac metodę, że chcemy przefiltrować np. te tokeny, ale jak je wyciągnąć stamtąd?
+   * - nie umiem je wyciagnąć z codeca, do zmiennych niemutowalnych
+   * - gdy próbuje do zmiennych mutowalnych, zmieniam zmienna mutowalna w srodku funkcji, to ta globalna zmienna mutowalna nie
+   *  zmienia swojej wartości
+    */
+
+  type StringListCodec[T] = Codec[List[String], T, CodecFormat.TextPlain]
+  def http[T: StringListCodec]: EndpointInput.Auth[T, EndpointInput.AuthType.Http] = {
+    val codec = implicitly[Codec[List[String], T, CodecFormat.TextPlain]]
+
+    var authScheme: String = ""
+
+    def filterHeaders[T: StringListCodec](headers: List[String]) =
+      headers.filter(x => x.toLowerCase.startsWith("token") || x.toLowerCase.startsWith("bearer"))
+
+    def stringPrefixWithSpace: Mapping[List[String], List[String]] =
+      Mapping.stringPrefixCaseInsensitiveForList(authScheme + " ")
+
+    val authCodec = Codec
+      .id[List[String], CodecFormat.TextPlain](codec.format, Schema.binary)
+      .map(x => {
+        val filteredHeaders = filterHeaders(x)
+        authScheme = filteredHeaders.head.split(" ").head.capitalize
+        filteredHeaders
+      })(identity)
+      .map(stringPrefixWithSpace)
+      .mapDecode(codec.decode)(codec.encode)
+      .schema(codec.schema)
+
+    EndpointInput.Auth(
+      header[T](HeaderNames.Authorization)(authCodec),
+      WWWAuthenticateChallenge(authScheme),
+      EndpointInput.AuthType.Http(authScheme),
+      EndpointInput.AuthInfo.Empty
+    )
+  }
 
   private def handleAuth(token: String): IO[ErrorInfo, UserSession] = {
     (for {
