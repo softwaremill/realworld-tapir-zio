dd8f2abc82a87b71580787a620d2b8a4044c21a9
wydra98 <wydra.adrian98@gmail.com>
2023-06-07 12:38:44 +0200

    Remove email type as separate file

--- src/main/scala/com/softwaremill/realworld/users/User.scala
+++ src/main/scala/com/softwaremill/realworld/users/User.scala
@@ -5,6 +5,7 @@ import com.softwaremill.realworld.common.domain.Username
 import sttp.tapir.{Schema, SchemaType}
 import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
 
+case class Email(value: String) extends AnyVal
 case class User(
     email: Email,
     token: Option[String],
@@ -16,3 +17,8 @@ case class User(
 object User:
   given userDataEncoder: zio.json.JsonEncoder[User] = DeriveJsonEncoder.gen[User]
   given userDataDecoder: zio.json.JsonDecoder[User] = DeriveJsonDecoder.gen[User]
+
+object Email:
+  given emailEncoder: JsonEncoder[Email] = JsonEncoder[String].contramap((c: Email) => c.value)
+  given emailDecoder: JsonDecoder[Email] = JsonDecoder[String].map(email => Email(email))
+  given emailSchema: Schema[Email] = Schema(SchemaType.SString())
